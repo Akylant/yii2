2cb78afc026da031bc15014e33e86dee716a6ab4
Akylant <Akylant@gmail.com>
2016-03-24 18:10:14 +0300

    change evernote for ldb lessons2

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=evernote',
+    'dsn' => 'mysql:host=localhost;dbname=lesson2',
     'username' => 'root',
     'password' => '12345',
     'charset' => 'utf8',
